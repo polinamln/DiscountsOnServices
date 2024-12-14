90974196210cba22ad2dffb386a36ef6f0d92da1
polinamln <polina77979.la@gmail.com>
2024-12-14 01:52:41 +0100

    1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,11 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Header() {
   return (
-    <nav className={css.header}>
-      <img className={css.logo} src="/logo.png" alt="logo"></img>
-      <RxHamburgerMenu className={css.burger} />
-    </nav>
+    <div className="container">
+      <nav className={css.header}>
+        <img className={css.logo} src="/logo.png" alt="logo"></img>
+        <RxHamburgerMenu className={css.burger} />
+      </nav>
+    </div>
   );
 }
